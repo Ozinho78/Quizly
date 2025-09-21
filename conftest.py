6601954eadaf1dd0080b88d866416041c88f54fa
Michael Fiebelkorn <michael.fiebelkorn@gmail.com>
2025-09-21 20:34:38 +0200

    pytest test reviewed

--- conftest.py
+++ conftest.py
@@ -1,13 +1,6 @@
-import pytest  # required to define shared fixtures
+import pytest
 
 
 def pytest_configure():
-    '''
-    Ensures Django knows this is a test run; place for any global test tweaks.
-    '''
-    # Intentionally left simple; extend if you need global settings overrides.
-    # For example, to force secure cookies off in tests, you could tweak settings here.
-    # from django.conf import settings
-    # settings.SESSION_COOKIE_SECURE = False
-    # settings.CSRF_COOKIE_SECURE = False
+    """Ensures Django knows this is a test run; place for any global test tweaks"""
     pass
